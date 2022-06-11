6873abcebc4a9e7843510c35cf954ab80cfdfa96
ansonglin <ansonglin@gome.com.cn>
2022-06-11 20:35:49 +0800

    project init

---
+++ soa-transaction-api/src/main/java/com/gome/pop/soatransactionapi/SoaTransactionApiApplication.java
@@ -0,0 +1,13 @@
+package com.gome.pop.soatransactionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaTransactionApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoaTransactionApiApplication.class, args);
+    }
+
+}
---
+++ soa-transaction-logic/src/test/java/com/gome/pop/soainterface/SoaInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gome.pop.soatransactionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoaInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
